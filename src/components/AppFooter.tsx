0fd43a3f5ba01a8312eceafbfae0f79dc954522c
Todd Garrison <toddlgarrison@gmail.com>
2023-09-18 14:13:20 -0400

    Set up Footer.tsx add Linkedin link

---
+++ src/components/AppFooter.tsx
@@ -0,0 +1,16 @@
+import { LinkedIn, GitHub, Summarize } from "@mui/icons-material"
+
+
+const AppFooter = () => {
+    return (
+        <div className="footer">
+            <div className="social-media">
+                <a href="https://https://www.linkedin.com/in/toddlgarrison/">
+                    <LinkedIn />
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default AppFooter
