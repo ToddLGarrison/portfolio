6844083ceda1878f0f7ed770a94cb0f8007258fd
Todd Garrison <toddlgarrison@gmail.com>
2023-09-18 15:05:22 -0400

    Add links and text to AppFooter.tsx

--- src/components/AppFooter.tsx
+++ src/components/AppFooter.tsx
@@ -1,14 +1,23 @@
-import { LinkedIn, GitHub, Summarize } from "@mui/icons-material"
+import { LinkedIn, GitHub, Summarize, Email } from "@mui/icons-material"
 
 
 const AppFooter = () => {
     return (
         <div className="footer">
             <div className="social-media">
-                <a href="https://https://www.linkedin.com/in/toddlgarrison/">
+                <a href="https://www.linkedin.com/in/toddlgarrison/" target="_blank" rel="noopener noreferrer">
                     <LinkedIn />
                 </a>
+
+                <a href="https://github.com/ToddLGarrison" target="_blank" rel="noopener noreferrer">
+                    <GitHub />
+                </a>
+
+                <a href="mailto:toddlgarrison@gmail.com">
+                    <Email />
+                </a>
             </div>
+            <p>All materials &copy; Todd Garrison 2023</p>
         </div>
     )
 }
